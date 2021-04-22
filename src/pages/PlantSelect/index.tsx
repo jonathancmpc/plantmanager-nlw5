56533cd9fa6749a841e02d05b9832a712643c754
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-22 07:36:15 -0300

    Bug resolvido - carregamento duplicado de plantas

--- src/pages/PlantSelect/index.tsx
+++ src/pages/PlantSelect/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import api from '../../services/api';
 
 import { Header } from '../../components/Header';
@@ -17,6 +17,7 @@ import {
   ContainerListCardPLants
 } from './styles';
 import { Load } from '../../components/Load';
+import colors from '../../styles/colors';
 
 interface EnviromentProps {
   key: string;
@@ -42,6 +43,29 @@ export function PlantSelect() {
   const [filteredPlants, setFilteredPlants] = useState<PlantsProps[]>([]);
   const [enviromentSelected, setEnviromentSelected] = useState('all');
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false);
+
+  async function fetchPlants() {
+    const { data } = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+    
+    if(!data) {
+      return setLoading(true);
+    }
+
+    if(page > 1) {
+      setPlants(oldValuePlants => [...oldValuePlants, ...data ]);       
+      setFilteredPlants(oldValuePlants => [...oldValuePlants, ...data ]);     
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+
+    
+    setLoading(false);
+    setLoadingMore(false);
+  }
 
   useEffect(() => {
     async function fetchEnviroment() {
@@ -60,13 +84,6 @@ export function PlantSelect() {
   }, []);
 
   useEffect(() => {
-    async function fetchPlants() {
-      const { data } = await api.get('plants?_sort=name&_order=asc');
-      setPlants(data);
-      setFilteredPlants(data);
-      setLoading(false);
-    }
-
     fetchPlants();
   }, []);
 
@@ -82,6 +99,16 @@ export function PlantSelect() {
     );
 
     setFilteredPlants(filtered);
+  }
+
+  function handleFetchMore(distance: number) {
+    if(distance < 1) {
+      return
+    }
+
+    setLoadingMore(true);
+    setPage(oldValuePage => oldValuePage + 1);
+    fetchPlants();
   }
 
 
@@ -127,6 +154,16 @@ export function PlantSelect() {
           )}
           showsVerticalScrollIndicator={false}
           numColumns={2}
+          onEndReachedThreshold={0.1} //Usuário chega a 10% da tela chama a função handleFetchMore.
+          onEndReached={({ distanceFromEnd }) => 
+            handleFetchMore(distanceFromEnd)
+          }
+          // Faz aparecer um loading(ActivityIndicator) quando rola a página enquanto carrega os novos itens na lista.
+          ListFooterComponent={
+            loadingMore 
+            ? <ActivityIndicator color={colors.green} />
+            : <></>
+          }
         />
       </ContainerListCardPLants>
 
