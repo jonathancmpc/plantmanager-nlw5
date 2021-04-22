0f7488e0ae76f374e7a60a07e89456b85ffecdce
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-22 15:06:11 -0300

    Transformando Confirmation em pagina dinamica

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 
 import { Button } from '../../components/Button';
 
 import { fonts } from '../../styles/fonts';
 import { Container, ContainerButton, Content, Emoji, Title, SubTitle } from './styles';
 
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: 'smile' | 'hug';
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: '🤗',
+  smile: '😄'
+}
+
 export function Confirmation() {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
+  const routes = useRoute();
+
+  const {
+    title,
+    subtitle,
+    icon,
+    buttonTitle,
+    nextScreen
+  } = routes.params as Params;
 
   function handleToStart() {
-    navigation.navigate('PlantSelect');
+    navigation.navigate(nextScreen);
   }
 
   return (
     <Container>
       <Content>
         <Emoji>
-          😄
+          {emojis[icon]}
         </Emoji>
 
         <Title style={fonts.Jost_600SemiBold}>
-          Prontinho
+          {title}
         </Title>
 
         <SubTitle style={fonts.Jost_400Regular}>
-          Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
+          {subtitle}
         </SubTitle>
       
         <ContainerButton>
-          <Button onPress={handleToStart} title="Começar" />
+          <Button onPress={handleToStart} title={buttonTitle} />
         </ContainerButton>
       </Content>
 
---
+++ src/pages/MyPlants/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px 30px 0 30px;
+  background-color: ${colors.background}
+`;
